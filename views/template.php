7fd4e0836c376385c1a24b83ca9f2cc384d7b20c
YaeH-code <uhm.yae@gmail.com>
2024-07-11 10:51:42 +0200

    Develop (#15)
    * Comment class and manager created
    
    * Category class and manager added to project
    
    * Post view page added to display list of articles by category
    
    * Git normalization and global style
    
    * User class and manager added to the project
    
    * Authentication controller added
    
    * Login and account creation view page created
    
    * user and admine view page added with .gitignore file
    
    * correction of empty database diplay (#9)
    
    * Auth fix (#10)
    
    * User's route manage with switchCase
    
    * Toggle password visibility function added
    
    * Image added for articles
    
    * Create README.md (#11)
    
    * database : SQL file added to create database with some testing data (#12)
    
    * Update : Readme installation part (#13)
    
    * Fix : global css and Js file path corrected into template file
    
    * XDebug json file added

--- views/template.php
+++ views/template.php
@@ -11,9 +11,9 @@
     <!-- googlefont/css -->
     <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
     <!-- global/css -->
-    <link rel="stylesheet" type="text/css" href="../public/css/global.css">
+    <link rel="stylesheet" type="text/css" href="./public/css/global.css">
     <!-- global/JS -->
-    <script src="../public/JS/globalJS.js"></script>
+    <script src="./public/JS/globalJS.js"></script>
     <!-- icons/fontawesome -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
